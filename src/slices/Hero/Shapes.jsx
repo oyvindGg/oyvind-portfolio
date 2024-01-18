593a04200c3976462a615e8413e5a7fc2cc6fc08
0yvz <oyvindg@live.no>
2024-01-18 22:38:54 +0100

    Add Shapes component for Hero slice

---
+++ src/slices/Hero/Shapes.jsx
@@ -0,0 +1,19 @@
+"use client"
+
+import * as THREE from "three"
+import { Canvas } from "@react-three/fiber"
+import { ContactShadows, Float, Enviroment } from "@react-three/drei"
+import { Suspense, useRef, useState, useEffect } from "react"
+import { gsap } from "gsap"
+
+export function Shapes(){
+	return (
+		<div className="row-span-1 row-start-1 -mt-9 aspect-square md:col-span-1 md:col-start-2 md:mt-0">
+			<Canvas className="z-0" shadows gl={{antialias:false}} dpr={[1, 1.5]} camera={{position: [0, 0, 25], fov:30, near:1, far:40}}>
+				<Suspense fallback={null}>
+					<ContactShadows position={[0, -3.5, 0]}/>
+				</Suspense>
+			</Canvas>
+		</div>
+	)
+}
