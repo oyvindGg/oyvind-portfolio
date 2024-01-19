2a4f484feea947365f14616a3413203f42463c28
0yvz <oyvindg@live.no>
2024-01-19 18:28:45 +0100

    Add Geometries component to Shapes.jsx

--- src/slices/Hero/Shapes.jsx
+++ src/slices/Hero/Shapes.jsx
@@ -17,6 +17,7 @@ export function Shapes(){
 			 camera={{ position: [0, 0, 25], fov: 30, near: 1, far: 40 }}
 		  >
 			 <Suspense fallback={null}>
+				<Geometries />
 				<ContactShadows
 				  position={[0, -3.5, 0]}
 				  opacity={0.65}
@@ -44,17 +45,75 @@ export function Shapes(){
 		new THREE.MeshNormalMaterial(),
 	]
 	
-	// pass to geometry
+	return geometries.map(({position, r, geometry}) => (
+		<Geometry 
+		key={JSON.stringify(position)} 
+		position={position.map((p) => p * 2)} 
+		r={r} 
+		geometry={geometry} 
+		materials={materials} />
+	))
 }
 
 function Geometry({r, position, geometry, materials}){
-	const mashRef = useRef()
+	const meshRef = useRef()
 	const [visible, setVisible] = useState(false)
 
 	const startingMaterial = getRandomMaterial()
-	
+
 		function getRandomMaterial(){
 			return gsap.utils.random(materials)
 		}
 	
+		function handleCLick(e){
+			const mesh = e.object;
+
+			gsap.to(mesh.rotation, {
+				x: `+=${gsap.utils.random(0,2)}`,
+				y: `+=${gsap.utils.random(0,2)}`,
+				z: `+=${gsap.utils.random(0,2)}`,
+				duration: 1.3,
+				ease: "elastic.out(1, 0.3)",
+				yoyo: true,
+			})
+			mesh.material = getRandomMaterial()
+		}
+
+		const handlePointerOver = () => {
+			document.body.style.cursor = "pointer"
+		}
+		const handlePointerOut = () => {
+			document.body.style.cursor = "default"
+		}
+
+		useEffect(() => {
+			gsap.context(() => {
+				setVisible(true)
+				gsap.from(meshRef.current.scale, 
+					{
+						x: 0,
+						y: 0,
+						z: 0,
+						duration: 1,
+						ease: "elastic.out(1, 0.3)",
+					})
+			});
+			return () => cubeTexture.revert();
+		}, [])
+
+		return (
+			<group position={position} ref={meshRef}>
+				<Float speed={5 * r} rotationIntensity={6 * r} floatIntensity={5 * r}>
+					<mesh
+						geometry={geometry}
+						material={startingMaterial}
+						onClick={handleCLick}
+						onPointerOver={handlePointerOver}
+						onPointerOut={handlePointerOut}
+						visible={visible}
+					/>
+				</Float>
+
+			</group>
+		)
 }
