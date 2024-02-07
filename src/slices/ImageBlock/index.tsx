dc0ae6195a90b29130d30234a7b477e769dee87f
0yvz <oyvindg@live.no>
2024-02-07 11:48:28 +0100

    Add PrismicNextImage component to ImageBlock slice

--- src/slices/ImageBlock/index.tsx
+++ src/slices/ImageBlock/index.tsx
@@ -1,4 +1,5 @@
 import { Content } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
 import { SliceComponentProps } from "@prismicio/react";
 
 /**
@@ -10,15 +11,9 @@ export type ImageBlockProps = SliceComponentProps<Content.ImageBlockSlice>;
  * Component for "ImageBlock" Slices.
  */
 const ImageBlock = ({ slice }: ImageBlockProps): JSX.Element => {
-  return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      Placeholder component for image_block (variation: {slice.variation})
-      Slices
-    </section>
-  );
+	return (
+		<PrismicNextImage field={slice.primary.image} imgixParams={{ w: 600 }} />
+	);
 };
 
 export default ImageBlock;
