09ff1b4a1c8d52245e748deb722cc7c3f9411950
0yvz <oyvindg@live.no>
2024-02-07 11:44:47 +0100

    Add dynamic import for ImageBlock component

--- src/slices/index.ts
+++ src/slices/index.ts
@@ -6,6 +6,7 @@ export const components = {
   biography: dynamic(() => import("./Biography")),
   content_index: dynamic(() => import("./ContentIndex")),
   hero: dynamic(() => import("./Hero")),
+  image_block: dynamic(() => import("./ImageBlock")),
   techlist: dynamic(() => import("./Techlist")),
   text_block: dynamic(() => import("./TextBlock")),
 };
